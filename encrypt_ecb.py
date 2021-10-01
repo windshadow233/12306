412659af2c6d59ff1b8cc4f5afe76e41524e166c
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2021-10-02 01:20:24 +0800

    Create encrypt_ecb.py

---
+++ encrypt_ecb.py
@@ -0,0 +1,14 @@
+from gmssl.sm4 import CryptSM4, SM4_ENCRYPT
+import base64
+
+key = b'tiekeyuankp12306'
+value = b'123' #  bytes类型
+iv = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+crypt_sm4 = CryptSM4()
+crypt_sm4.set_key(key, SM4_ENCRYPT)
+
+
+def encrypt_passwd(password):
+    encrypted_passwd = crypt_sm4.crypt_ecb(password.strip().encode())
+    encrypted_passwd = base64.b64encode(encrypted_passwd).decode()
+    return "@" + encrypted_passwd
