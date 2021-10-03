6cd0328014970213e2c5ae9a19bab485da5c7bf0
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2021-10-04 02:13:20 +0800

    Update cmd_tools.py

--- cmd_tools.py
+++ cmd_tools.py
@@ -1,41 +1,58 @@
-import argparse
+import cmd2, sys
 from ticket_bot import RailWayTicket
 
-parser = argparse.ArgumentParser(description='Search for tickets information')
-parser.add_argument('-s', '--start', type=str, help='Start Station')
-parser.add_argument('-e', '--end', type=str, help='Arrive Station')
-parser.add_argument('-d', '--date', type=str, help='Date (format: %Y%m%d, eg: 20210512)', default=None)
-parser.add_argument('-t', '--type', type=str.upper, help='Type of Train',
-                    choices=['G', 'D', 'K', 'T', 'Z', None], default=None)
-parser.add_argument('-l', '--login', type=str.upper, help='Whether to Login', choices=['Y', 'N'], default='N')
-parser.add_argument('-m', '--login_method', type=str, help='Method for Login', choices=['qr', 'sms'], default='qr')
-parser.add_argument('-u', '--user', type=str, help='User Name (Your Phone Number)')
-parser.add_argument('-p', '--password', type=str, help='Your Password')
-parser.add_argument('-c', '--cast_num', type=str, help='Last 4 digits of Your ID Card')
 
-args = parser.parse_args()
+class TicketBotShell(cmd2.Cmd):
+    intro = 'Welcome to the 12306 ticket bot shell. Type help or ? to list commands.\n'
+    prompt = '(12306)>'
+    bot = RailWayTicket()
 
-bot = RailWayTicket()
+    search_parser = cmd2.Cmd2ArgumentParser(description='Search for tickets information')
+    search_parser.add_argument('-s', '--start', type=str, help='Start Station')
+    search_parser.add_argument('-e', '--end', type=str, help='Arrive Station')
+    search_parser.add_argument('-d', '--date', type=str,
+                               help='Date (format: %%Y%%m%%d, eg: 20210512, default: today)', default=None)
+    search_parser.add_argument('-t', '--type', type=str.upper, help='Type of Train (The first letter of train number)',
+                               choices=['G', 'D', 'K', 'T', 'Z', None], default=None)
 
-if args.login == 'Y':
-    if args.login_method == 'sms':
-        if not args.user:
-            print('未提供手机号!(-u)')
-            exit(-1)
-        if not args.password:
-            print('未提供密码!(-p)')
-            exit(-1)
-        if not args.cast_num:
-            print('未提供身份证后四位!(-c)')
-            exit(-1)
-        bot.sms_login(args.user, args.password, args.cast_num)
-    else:
-        bot.qr_login()
-if args.date is not None:
-    date = f'{args.date[:4]}-{args.date[4: 6]}-{args.date[6:]}'
-tickets = bot.get_ticket_info(from_=args.start, to_=args.end, date=date, train_type=args.type)
-if tickets:
-    bot.print_ticket_info(tickets)
-else:
-    print('未找到票源,请更改日期或站名~')
+    @cmd2.with_argparser(search_parser)
+    def do_search(self, args):
+        if args.date is not None:
+            args.date = f'{args.date[:4]}-{args.date[4: 6]}-{args.date[6:]}'
+        tickets = self.bot.get_ticket_info(args.start, args.end, args.date, args.type)
+        if tickets:
+            self.bot.print_ticket_info(tickets)
+        else:
+            print('未找到票源,请更改日期或站名~')
 
+    login_parser = cmd2.Cmd2ArgumentParser(description='Get login')
+    login_parser.add_argument('-m', '--login_method', type=str, help='Method for Login.\n'
+                                                                     'qr: to login with QR code\n'
+                                                                     'sms: to login with sms code(Usage count is limited in a day)',
+                              choices=['qr', 'sms'], default='qr')
+    login_parser.add_argument('-u', '--user', type=str, help='User Name (Your Phone Number)')
+    login_parser.add_argument('-p', '--password', type=str, help='Your Password')
+    login_parser.add_argument('-c', '--cast_num', type=str, help='Last 4 digits of Your ID Card')
+
+    @cmd2.with_argparser(login_parser)
+    def do_login(self, args):
+        if args.login_method == 'sms':
+            if not args.user:
+                print('未提供手机号!(-u)')
+                exit(-1)
+            if not args.password:
+                print('未提供密码!(-p)')
+                exit(-1)
+            if not args.cast_num:
+                print('未提供身份证后四位!(-c)')
+                exit(-1)
+            self.bot.sms_login(args.user, args.password, args.cast_num)
+        else:
+            self.bot.qr_login()
+
+    def do_is_login(self, args):
+        print(self.bot.check_login())
+
+
+if __name__ == "__main__":
+    TicketBotShell().cmdloop()
