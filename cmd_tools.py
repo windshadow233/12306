3868a25e8b90bfa61a75e74c00eb2a681268b0e7
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2021-10-05 03:32:29 +0800

    Update cmd_tools.py

--- cmd_tools.py
+++ cmd_tools.py
@@ -1,4 +1,5 @@
 import cmd2
+import sys
 from ticket_bot import RailWayTicket
 
 
@@ -16,16 +17,31 @@ class TicketBotShell(cmd2.Cmd):
                                help='Date (format: %%Y%%m%%d, eg: 20210512, default: today)', default=None)
     search_parser.add_argument('-t', '--type', type=str.upper, help='Type of Train (The first letter of train number)',
                                choices=['G', 'D', 'K', 'T', 'Z', None], default=None)
+    search_parser.add_argument('-a', '--all', type=str.upper, help='Whether to show tickets which have been sold out.',
+                               choices=['Y', 'N'], default='N')
 
     @cmd2.with_argparser(search_parser)
     def do_search(self, args):
         if args.date is not None:
             args.date = f'{args.date[:4]}-{args.date[4: 6]}-{args.date[6:]}'
-        self.tickets = self.bot.get_ticket_info(args.start, args.end, args.date, args.type)
+        self.tickets = self.bot.get_ticket_info(args.start, args.end, args.date, args.type, args.all == 'Y')
         if self.tickets:
             self.bot.print_ticket_info(self.tickets)
         else:
             print('未找到票源,请更改日期或站名~')
+
+    show_parser = cmd2.Cmd2ArgumentParser(description='Show something')
+    show_parser.add_argument('list', type=str, choices=['tickets', 'passengers'])
+
+    @cmd2.with_argparser(show_parser)
+    def do_show(self, args):
+        if args.list == 'tickets':
+            if not self.tickets:
+                print('There is no tickets saved in cache.')
+                return
+            self.bot.print_ticket_info(self.tickets)
+        else:
+            self._show_passengers()
 
     login_parser = cmd2.Cmd2ArgumentParser(description='Get login')
     # login_parser.add_argument('-m', '--method', type=str,
@@ -54,29 +70,40 @@ class TicketBotShell(cmd2.Cmd):
         # else:
         self.bot.qr_login()
 
-    def do_show_passengers(self, args):
+    def _show_passengers(self):
+        """Show your related passengers if you're login."""
         if not self.bot.check_login():
             print('Please login first.')
             return
-        self.passengers = self.bot.get_passengers()
+        if self.passengers:
+            print("Read passengers'data from cache.\n(Use 'clear' cmd if you want to reload them)")
+        else:
+            self.passengers = self.bot.get_passengers()
         self.bot.print_passengers(self.passengers)
+
+    clear_parser = cmd2.Cmd2ArgumentParser(description="Clear some list attributes.")
+    clear_parser.add_argument("list", type=str, choices=['passengers', 'tickets'])
+
+    @cmd2.with_argparser(clear_parser)
+    def do_clear(self, args):
+        self.__getattribute__(args.list).clear()
 
     def do_logout(self, args):
         """Get logout."""
         self.bot.logout()
+        self.passengers.clear()
 
     def do_is_login(self, args):
         """Check whether you're login or not."""
         print(self.bot.check_login())
 
-    def do_quit(self, args):
-        """Quit the shell."""
+    def do_bye(self, args):
+        """Say bye to the shell."""
         print('Thank you for using 12306 ticket bot shell.\nBye~')
         return True
 
-    def do_bye(self, args):
-        return self.do_quit(args)
+    do_quit = do_bye
 
 
 if __name__ == "__main__":
-    TicketBotShell().cmdloop()
+    sys.exit(TicketBotShell().cmdloop())
